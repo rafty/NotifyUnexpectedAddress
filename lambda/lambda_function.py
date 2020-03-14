1b1b07614010cc67bf48ed5771fac97994f15c14
yagitatakashi <yagita.takashi@gmail.com>
2020-03-14 20:57:09 +0900

    1. To filter destination IP addresses with AWS_CIDR  Add: AWS_CIDR 2. To filter by CIDR whitelist  Add: WhiteListCIDR

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,6 +3,8 @@ import os
 import time
 import socket
 import urllib.parse
+import urllib.request
+import urllib.error
 import csv
 import gzip
 import ipaddress
@@ -11,7 +13,7 @@ import logging
 import boto3
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
 
 s3 = boto3.client('s3')
 ssm = boto3.client('ssm')
@@ -22,26 +24,72 @@ LOGS_GROUP_NAME = os.environ['LOGS_GROUP_NAME']
 LOGS_STREAM_NAME = os.environ['LOGS_STREAM_NAME']
 
 # local cidr
+# VPC_CIDR = [
+#     '10.0.0.0/8',
+#     '172.16.0.0/12',
+#     '192.168.0.0/16',
+#     '198.19.0.0/16']
 VPC_CIDR = [
-    '10.0.0.0/8',
-    '172.16.0.0/12',
-    '192.168.0.0/16',
-    '198.19.0.0/16']
+    '10.254.240.0/24',
+    '10.254.241.0/26',
+    '10.254.242.0/26']
+
+AWS_IP_RANGE_URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+
+
+# ----------------------------------------------------------------------
+# AWS IP Range
+# ----------------------------------------------------------------------
+def get_aws_cidr():
+    try:
+        with urllib.request.urlopen(AWS_IP_RANGE_URL) as res:
+            body = res.read().decode('utf-8')
+            cidr = json.loads(body)
+            return cidr
+    except urllib.error.URLError as e:
+        logger.info('Error: get_aws_ip_ranges() {}'.format(str(e)))
+        return {}
+
+
+def extract_aws_ip_range(cidr):
+    regions = ['ap-northeast-1', 'us-east-1']
+    cidr_list = [_range.get('ip_prefix') for _range in cidr.get('prefixes')
+                 if _range.get('region') in regions]
+    return cidr_list
+
+
+cidr = get_aws_cidr()
+AWS_CIDR = set(extract_aws_ip_range(cidr))
+AWS_CIDR = sorted(AWS_CIDR)
+
+
+def in_white_list_cidr():
+    response = ssm.get_parameter(Name='whitelist_cidr_v1')
+    white_list = response.get('Parameter').get('Value').split(',')
+    return white_list
+
+WHITE_LIST_CIDR = in_white_list_cidr()
 
 
 def reverse_lookup(ip):
     try:
         return socket.gethostbyaddr(ip)[0]
     except socket.herror as e:
-        logger.error(e)
+        # logger.error(e)
+        logger.info('illegal.domain: gethostbyaddr() {}'.format(str(e)))
         return 'illegal.domain'
 
 
 def in_local_cidr(ip_addr):
 
-    ip = ipaddress.ip_address(ip_addr)
+    cidr_list = VPC_CIDR + AWS_CIDR + WHITE_LIST_CIDR
+    try:
+        ip = ipaddress.ip_address(ip_addr)
+    except ValueError:
+        # for example. ipaddr: '-'
+        return True
 
-    for cidr in VPC_CIDR:
+    for cidr in cidr_list:
         nw = ipaddress.ip_network(cidr)
         if ip in nw:
             logger.info('ip: {} in local cidr: {}'.format(ip_addr, cidr))
@@ -53,8 +101,8 @@ def in_local_cidr(ip_addr):
     return False
 
 
-def in_white_list(domain):
-    response = ssm.get_parameter(Name='whitelist_for_external_domain')
+def in_white_list_domain(domain):
+    response = ssm.get_parameter(Name='whitelist_for_external_domain_v1')
     white_list = response.get('Parameter').get('Value').split(',')
     logger.info('whitelist: {}'.format(white_list))
     logger.info('whitelist type: {}'.format(type(white_list)))
@@ -107,14 +155,15 @@ def message_format(messages):
 
 
 def check_domain(dest, line):
+
     domain = reverse_lookup(dest)
     logger.info('domain: {}'.format(domain))
 
-    if in_white_list(domain):
+    if in_white_list_domain(domain):
         logger.info('no problem domain: {} <- ip: {}'.format(domain, dest))
     else:
         # alarm
-        logger.info('Alarm domain: {} <- ip: {}'.format(domain, dest))
+        logger.warning('Alarm domain: {} <- ip: {}'.format(domain, dest))
         message = 'Alarm domain: {}, ip-address: {}'.format(domain, dest)
         log_line = json.dumps(line)
         _message = message_format([message, log_line])
@@ -141,9 +190,6 @@ def lambda_handler(event, context):
             for line in f:
                 dest = line.get('dstaddr')
                 logger.info('dest ip: {}'.format(dest))
-
-                # dest = '95.211.80.227'  # test
-                # dest = '0.0.0.0'  # test
 
                 if not in_local_cidr(dest):
                     check_domain(dest, line)
